17c3480dd8b394f2577c46ff84d48d84fea316ac
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-09 08:24:21 -0800

    edited

--- ecommerceproject/ecommerceproject/settings.py
+++ ecommerceproject/ecommerceproject/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'ecommerceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER' : 'root',
+        'PASSWORD' :'',
     }
 }
 
@@ -117,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ecommerceproject/shop/urls.py
+++ ecommerceproject/shop/urls.py
@@ -3,3 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.main,name='main')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
