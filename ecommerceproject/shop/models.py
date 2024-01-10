1fa73bf77959fe6c5a539e721144ff0fdee67816
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-10 06:21:18 -0800

    modified

--- ecommerceproject/shop/models.py
+++ ecommerceproject/shop/models.py
@@ -2,11 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-
-    name=models.CharField(max_length=250)
-    desc=models.TextField()
-    image=models.ImageField(upload_to='img')
-    price=models.DecimalField(max_digits=10, decimal_places=2)
+	name= models.CharField(max_length=250, unique=True)
+	slug=models.SlugField(max_length=250,unique=True)
 
     def __str__(self):
         return 
