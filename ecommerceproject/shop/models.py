86ad2260be2cc0e7267feeade2f793d8285d0053
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-10 07:23:31 -0800

    modified

--- ecommerceproject/shop/models.py
+++ ecommerceproject/shop/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Products(models.Model):
 	name= models.CharField(max_length=250, unique=True)
 	slug=models.SlugField(max_length=250,unique=True)
-
+	desc=models.TextField(blank=True)
+	image=models.ImageField(upload_to='cat_img',blank=True)
     def __str__(self):
         return 
