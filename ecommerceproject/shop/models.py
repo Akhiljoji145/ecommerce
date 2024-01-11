8c9369f553d3eb49b28ad32b1b55171ec2881c8f
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-10 21:08:10 -0800

    modified

--- ecommerceproject/shop/models.py
+++ ecommerceproject/shop/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Category(models.Model):
 	name= models.CharField(max_length=250, unique=True)
 	slug=models.SlugField(max_length=250,unique=True)
 	desc=models.TextField(blank=True)
 	image=models.ImageField(upload_to='cat_img',blank=True)
     def __str__(self):
         return 
-class 
+class Products(models.Model)
