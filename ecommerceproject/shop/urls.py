bc8cbaa4e91c671ff5fe4cdb2793992536f665fb
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-13 19:12:46 -0800

    modified

--- ecommerceproject/shop/urls.py
+++ ecommerceproject/shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.main,name='main')
+    path('',views.,name='main')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ecommerceproject/shop/views.py
+++ ecommerceproject/shop/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
-def main(request):
-	return render(request,'index.html')
+from .models import Category,Products
+def allProdCat(request,c_slug=None):
+	c_page=None
+	products=None
+	if c_slug!=None:
+		c_page=get_object_or_404(Category,slug=c_slug)
+		products=Products.objects.all().filter(category=c_page,available=True)
+	else:
+		products=Products.objects.all().filter(available=True)
+	return render(request,'category.html',{'category':category,'products':products})
