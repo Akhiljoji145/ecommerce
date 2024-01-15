bf4c37044fd76ff5a26c42c9e91d075ed5412a6c
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-15 07:54:08 -0800

    modified

--- ecommerceproject/shop/urls.py
+++ ecommerceproject/shop/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.allProdCat,name='main')
+    path('',views.allProdCat,name='main'),
+    path('<slug:c_slug>',views.allProdCat,name='category'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
