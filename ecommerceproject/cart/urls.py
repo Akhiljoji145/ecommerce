7cc0358a1107cece57fdf0af278d0d5beb1404ba
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-02-10 13:21:52 +0530

    modified

--- ecommerceproject/cart/urls.py
+++ ecommerceproject/cart/urls.py
@@ -5,4 +5,6 @@ app_name='cart'
 urlpatterns = [
 	path('add/<int:product_id>/',views.add_cart,name='add_cart'),
     path('',views.cart_details,name='cart_details'),
+    path('delete/<int:product_id>/',views.cart_delete,name='cart_delete'),
+    path('remove/<int:product_id>/',views.full_remove,name='remove'),
 ]
